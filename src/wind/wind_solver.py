aa1fd4e05406448fb0d5567a26e8f07db7f28742
Rahim Mustaqim, MSc (Eng.) <rahimmustaqim37@gmail.com>
2025-12-30 17:46:10 +0900

    Update wind_solver.py

--- src/wind/wind_solver.py
+++ src/wind/wind_solver.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class WindSolver:
     def __init__(self, wind_speed):
         self.wind_speed = wind_speed
@@ -6,5 +8,10 @@ class WindSolver:
         rho = 1.225
         area = 200.0
         Ct = 0.8
-        force = 0.5 * rho * Ct * area * self.wind_speed**2
+        thrust = 0.5 * rho * Ct * area * self.wind_speed**2
+
+        force = np.zeros(6)
+        force[0] = thrust      # surge
+        force[4] = thrust * 0.02  # pitch moment (simplified)
+
         return force
